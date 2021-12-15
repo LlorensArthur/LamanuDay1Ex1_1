93d58958c1ce4b2e07c7dc34546e6d6ac45266d0
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 11:36:46 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int firstNumber = 2;
+int secondNumber = 3;
+int thirdNumber = firstNumber + secondNumber;
+Console.WriteLine("La valeur du premier nombre est " + firstNumber + ".");
+Console.WriteLine("La valeur du deuxième nombre est " + secondNumber + ".");
+// Affichage de la somme des deux nombres.
+Console.WriteLine("L'addition des deux nombres fait " + thirdNumber + ".");
